3f79ae9d8cad545dc0178a58cfc74a8416df2aa4
judemanish283 <108987548+judemanish283@users.noreply.github.com>
2024-11-15 15:30:26 +0530

    logic fix and spine removed

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 using SymbolInfo;
 using System;
 using UnityEditor.ShaderGraph.Internal;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
 
 public class GameController : MonoBehaviour
 {
@@ -23,7 +26,13 @@ public class GameController : MonoBehaviour
     [SerializeField] float MaxscrollDuration = 5f;
     [SerializeField] float MinscrollDuration = 3f;
     List<GameObject> ConsideredSymbols = new List<GameObject>();
-    GameObject[,] _ConsidSymb = new GameObject[3,5];
+    [SerializeField] GameObject[,] _ConsidSymb = new GameObject[5,3];
+
+    // This is the co-odinates by which the symbols are stored
+    // (0,0) (1,0) (2,0) (3,0) (4,0)
+    // (0,1) (1,1) (2,1) (3,1) (4,1)
+    // (0,2) (1,2) (2,2) (3,2) (4,2)
+
 
     private void Start()
     {
@@ -56,38 +65,44 @@ public class GameController : MonoBehaviour
             yield return new WaitForSeconds(spinDuration);
         }
         yield return new WaitForSeconds(1f);
-       ReturnConsideredSymbols();
+        ReturnConsideredSymbols();
     }
 
     List<GameObject> ReturnConsideredSymbols()
     {
-        foreach(var row in symbolsRows)
-        {
+        int row = 0;
+        int column = 0;
 
+        for (int i = 0; i < symbolsRows.Count && row < 5; i++) // Loop over the symbolsRows
+        {
+            for (int j = 0; j < 3 && row < 5; j++) // Loop to get the three children (indices 4, 5, 6)
+            {
+                // Get the child from the content and set its color to red
+                symbolsRows[i].content.GetChild(j + 4).gameObject.GetComponentInChildren<Image>().color = Color.red;
+                
+                // Assign the GameObject to the 2D array
+                _ConsidSymb[i, j] = symbolsRows[i].content.GetChild(j + 4).gameObject;
+
+                // Log the current object position and its coordinates
+                //Debug.Log("Obj" + i + "," + j + "    row :" + row + "   col : " + column, symbolsRows[i].content.GetChild(j + 4));
+
+                // Increment the column, and move to the next row if the end of the row is reached
+                column++;
+                if (column == 5) // Check if we reached the end of a 5-column row
+                {
+                    column = 0;
+                    row++; // Move to the next row
+                }
+            }
         }
 
-
-        //for (int i = 0; i < 5; i++)
-        //{  // Loop through the rows
-        //    Debug.Log("i");
-        //    for (int j = 0; j < 3; j++)
-        //    {  // Loop through the columns
-        //        Debug.Log("j"); 
-        //       // Instantiate a new GameObject and assign it to the array at position (i, j)
-        //        _ConsidSymb[i, j] = symbolsRows[i].content.GetChild(j).gameObject;
-        //    }
-        //}
         Debug.Log("Number of items is : " + _ConsidSymb.Length + "should be 15");
+        Debug.Log("4,1 game object name is : " + _ConsidSymb[4,1].gameObject.name, _ConsidSymb[4,1].gameObject);
+        Debug.Log("4,2 game object name is : " + _ConsidSymb[4,2].gameObject.name, _ConsidSymb[4,2].gameObject);
+        Debug.Log("3,2 game object name is : " + _ConsidSymb[3,2].gameObject.name, _ConsidSymb[3,2].gameObject);
+        Debug.Log("3,1 game object name is : " + _ConsidSymb[3,1].gameObject.name, _ConsidSymb[3,1].gameObject);
+        
 
-        foreach (var row in symbolsRows)
-        {
-            ConsideredSymbols.Add(row.content.GetChild(4).gameObject);
-            ConsideredSymbols.Add(row.content.GetChild(5).gameObject);
-            ConsideredSymbols.Add(row.content.GetChild(6).gameObject);
-            //Debug.Log(row.content.GetChild(4).gameObject.name, row.content.GetChild(4));
-            //Debug.Log(row.content.GetChild(5).gameObject.name, row.content.GetChild(5));
-            //Debug.Log(row.content.GetChild(6).gameObject.name, row.content.GetChild(6));
-        } 
         return ConsideredSymbols;
     }
 
